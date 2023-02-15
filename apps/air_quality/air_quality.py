d5afce1853709535c7f83cf3e88b1f3ed3ea6ed2
jtao <YdpmvVbR22$$##wagtj22>
2023-02-15 05:58:38 -0600

    updated file.s Jianx

--- apps/air_quality/air_quality.py
+++ apps/air_quality/air_quality.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('../../lib')
 from TAUVirtualSensor import VirtualSensor
 
-broker_address = "datahub.geos.tamu.edu"
+#broker_address = "datahub.geos.tamu.edu"
+broker_address = "localhost"
 vs = VirtualSensor(broker_address, filepath="air_quality.csv", delimiter=";", interval=5)
 vs.publish()
-
